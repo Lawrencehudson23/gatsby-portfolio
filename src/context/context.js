954c5c4933d515c278953ae40a833f25b7c94d05
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-07-24 15:40:13 -0700

    changed vid to small

--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-const PortfolioContext = React.createContext();
+const PortfolioContext = React.createContext({
+  //   title: 'Lawrence',
+  //   name: 'Lawrence',
+  //   subtitle: '',
+  //   cta: '',
+});
 
 export const PortfolioProvider = PortfolioContext.Provider;
 export const PortfolioConsumer = PortfolioContext.Consumer;
